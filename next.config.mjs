20a08fc7264b87008252f0d686c1a67daf204204
李云富 <liyunfu1998@qq.com>
2024-04-08 19:49:27 +0800

    feat: 数据库连接+登录注册

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BASE_URL: 'http://localhost:3000',
+    MONGODB_URL: 'mongodb://root:123456@47.99.147.11:27017/next-shopping?authSource=admin',
+    ACCESS_TOKEN_SECRET: '1233211234567890',
+    REFRESH_TOKEN_SECRET: '987654321',
+  },
+}
 
-export default nextConfig;
+export default nextConfig
