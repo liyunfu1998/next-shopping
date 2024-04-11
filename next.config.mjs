fa7ae50501303ade0594c7a4ac05058c8712caa0
李云富 <liyunfu1998@qq.com>
2024-04-11 15:28:30 +0800

    feat: 添加生产环境baseurl

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BASE_URL: 'http://localhost:3000',
+    BASE_URL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://shopping.liyunfu.tech'
+        : 'http://localhost:3000',
     MONGODB_URL: 'mongodb://root:123456@47.99.147.11:27017/next-shopping?authSource=admin',
     ACCESS_TOKEN_SECRET: '1233211234567890',
     REFRESH_TOKEN_SECRET: '987654321',
