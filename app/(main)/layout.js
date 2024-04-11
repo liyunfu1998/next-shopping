ae3dab2d9b78ed23d80753fd1166eeb1e356c51c
李云富 <liyunfu1998@qq.com>
2024-04-11 10:50:19 +0800

    feat: 添加部分通用组件

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,5 +1,13 @@
+'use client'
+import { Toaster } from 'react-hot-toast'
 import StoreProvider from '@/app/StoreProvider'
 
 export default function Layout({ children }) {
-  return <StoreProvider>{children}</StoreProvider>
+  return (
+    <StoreProvider>
+      {children}
+
+      <Toaster position="top-center" toastOptions={{ style: { fontSize: '1rem' } }} />
+    </StoreProvider>
+  )
 }
--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,8 +1,13 @@
 'use client'
-import { useSelector } from 'react-redux'
-
+import { toast } from 'react-hot-toast'
 export default function Page() {
-  const res = useSelector(store => store)
-  console.log('res', res)
-  return <div>hello</div>
+  const onHandleClick = () => {
+    console.log('aaaa')
+    toast.success('成功啦')
+  }
+  return (
+    <div>
+      <button onClick={onHandleClick}>点击我显示toast</button>
+    </div>
+  )
 }
